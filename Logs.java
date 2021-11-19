b5eecaf30ba1806797b30b05043f3cd71fa65a61
Ubuntu <ubuntu@ip-172-31-90-22.ec2.internal>
2021-11-19 17:32:08 +0000

    edited Logs.java

--- Logs.java
+++ Logs.java
@@ -5,11 +5,26 @@
 * @version 1.0
 * @since   2020-01-01
 */
+
 import java.util.Scanner;
+
 /**
 * This is the standard "Hello, World!" program.
 */
+
 final class Logs {
+
+    /**
+    * This is variable for the truck capacity.
+    */
+
+    public static final double CAPACITY = 1100;
+
+    /**
+    * This is variable for the log density.
+    */
+
+    public static final double LOG_DENSITY = 20;
 
     /**
     * Prevent instantiation.
@@ -19,6 +34,7 @@ final class Logs {
     * @throws IllegalStateException
     *
     */
+
     private Logs() {
         throw new IllegalStateException("Cannot be instantiated");
     }
@@ -28,16 +44,19 @@ final class Logs {
     *
     * @param args No args will be used
     */
-    Mass = 55;
+
     public static void main(final String[] args) {
-       final Scanner logLength = new Scanner(System.in);
-       System.out.print("Enter log length (0.25, 0.5, or 1): ");
-       final float length = logLength.nextFloat();
-       System.out.println("The truck will be able to take "
-		+ (Mass / length) + " logs that are " + length
-		+ " meter(s) in length.");
-       
-       System.out.println("\nDone.");
+        final Scanner logLength = new Scanner(System.in);
+
+        System.out.print("Enter log length (0.25, 0.5, or 1): ");
+
+        final float length = logLength.nextFloat();
+
+        System.out.println("The truck will be able to take "
+                + (CAPACITY / LOG_DENSITY) / length + " logs that are " + length
+                + " meter(s) in length.");
+
+        System.out.println("\nDone.");
     }
 }
 
